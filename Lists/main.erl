06bc211042aa692f3047860ca7c44fb29c4009b6
Romain Mencattini <romain.mencattini@etu.unige.ch>
2016-06-24 16:54:50 +0200

    ajout jusqu'au problème 1.11

--- Lists/main.erl
+++ Lists/main.erl
@@ -1,7 +1,8 @@
 -module(main).
 -export([last/1,last_but_one/1,nth/2]).
 -export([lengthMe/1,reverse/1,palindrome/1]).
--export([flatten/1,compress/1]).
+-export([flatten/1,compress/1,pack/1]).
+-export([encode/1,encode_modified/1]).
 
 %% 1.01 (*) Find the last element of a list.
 last([Element]) -> Element;
@@ -46,3 +47,35 @@ compress([Head], Result) -> [Head|Result];
 compress([Head,Head|Tail],Result) 
 	when Head == Head -> compress([Head|Tail], Result);
 compress([Head,Second|Tail],Result) -> compress([Second|Tail], [Head|Result]).
+
+%% 1.09 (**) Pack consecutive duplicates of list elements into sublists.
+pack(Liste) -> lists:reverse(pack(Liste,[])).
+
+pack([],Result) -> Result;
+pack(Liste,Result) -> 	[ResultTmp,NewTail] = small_pack(Liste,[]),
+						pack(NewTail,[ResultTmp]++Result).
+
+small_pack([Head],Result) -> [[Head|Result],[]];
+small_pack([Head,Second|Tail],Result)
+	when not(Head == Second) -> [[Head|Result],[Second|Tail]];
+small_pack([Head,Head|Tail], Result)
+	when Head == Head -> small_pack([Head|Tail],[Head|Result]).
+
+% 1.10 (*) Run-length encoding of a list.
+encode(Liste) -> 	NewListe = pack(Liste),
+					lists:reverse(encode(NewListe,[])).
+
+encode([],Result) -> Result;
+encode([Head|Tail],Result) -> 	[First|_] = Head,
+								Size = lengthMe(Head),
+								encode(Tail,[[Size,First]|Result]).
+
+%% 1.11 (*) Modified run-length encoding.
+encode_modified(Liste) -> 	NewListe = encode(Liste),
+							lists:reverse(encode_modified(NewListe,[])).
+
+encode_modified([],Result) -> Result;
+encode_modified([[Size,Element]|Tail],Result) 
+	when Size == 1 -> encode_modified(Tail,[Element|Result]);
+encode_modified([[Size,Element]|Tail],Result)
+	when Size > 1 -> encode_modified(Tail,[[Size,Element]|Result]).
